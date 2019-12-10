b9fef3ef7577c0d2f888b60c8168950d27f17a17
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 16:41:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,17 +13,12 @@ def reduce (array,starting_value=0)
     starting_value = 0 
   end 
   i = 0 
-  grand_total = 0 
   num = 0 
   while i < array.length do 
     num = yield(num,array[i])
-    # grand_total = yield(array[i],starting_value)
     i+=1 
   end 
-  
-  # yield(grand_total,starting_value)
   return num+starting_value
-  
 end 
 
 # Failure/Error: expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
