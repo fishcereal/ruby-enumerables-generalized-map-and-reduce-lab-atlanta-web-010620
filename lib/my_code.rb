9c5cd4f0b8ceddfdd457eb13a78a351b41114e24
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 16:36:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+def map(array)
+  i = 0 
+  while i < array.length do 
+    array[i] = yield(array[i])
+    i+=1 
+  end
+  return array 
+end
+
+def reduce (array,starting_value=0)
+  if starting_value == nil 
+    starting_value = 0 
+  end 
+  i = 0 
+  grand_total = 0 
+  while i < array.length do 
+    array[i] = yield(array[i],grand_total)
+    # grand_total = yield(array[i],starting_value)
+    i+=1 
+  end 
+  
+  # yield(grand_total,starting_value)
+  return grand_total+starting_value
+  
+end 
+
+# Failure/Error: expect(reduce(source_array){|memo, n| memo + n}).to eq(6)
