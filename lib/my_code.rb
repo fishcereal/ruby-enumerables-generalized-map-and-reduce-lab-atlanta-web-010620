fc26c658a59a6ba1f7b419cf2c8eff6848278d27
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-10 16:54:05 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(array)
+  i = 0 
+  while i < array.length do 
+    array[i] = yield(array[i])
+    i+=1 
+  end
+  return array 
+end
+
+def reduce (array,starting_value=nil)
+  if starting_value != nil 
+    num = starting_value
+  else 
+    num = 0 
+  end 
+  i = 0 
+  while i < array.length do 
+    num = yield(array[i],num)
+    i+=1 
+  end
+  if num == nil 
+    return false 
+  end
+  return num
+end 
